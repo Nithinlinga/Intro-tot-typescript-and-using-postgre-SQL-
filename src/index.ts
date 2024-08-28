19153770c2ef805b9ac360fa26008e201084ea0e
Nithinlinga <nithinlinga2@gamil.com>
2024-08-28 23:04:18 +0530

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -26,53 +26,23 @@ const client=new Client({
 
 
 
-// async function getData() {
-//     try {
-//         // const string: insertQuery=""
-//         await client.connect();
-//         const email="nithinlinga46@gmail.com"
-//         const query=`SELECT * FROM  users where firstname=nithin`;
-        
-//         const result=await client.query(query);
-//         if(result.rows.length>0){
-//             console.log("User found:",result.rows);
-//             return result.rows[0];
-
-//         }
-//         else{
-//             console.log("User not found:");
-//             return null;
-//         }
-
-//     } catch (error) {
-//         console.log(error);
-//         throw error;
-//     }
-//     finally{
-//         await client.end();
-//     }
-// }
-// getData().catch(console.error);
-
-async function getuserData(){
+async function getData() {
     try {
-        await  client.connect();
-        const query=`SELECT u.id,u.firstname,u.lastname,u.email,a.village,a.district,a.state,a.pincode
-                     FROM users u
-                     JOIN addresses a ON u.id=a.user_id
-                     WHERE u.id=1;
-        `
-
-        const result=await client.query(query);
+        // const string: insertQuery=""
+        await client.connect();
+        // const email="nithinlinga46@gmail.com"
+        const query='SELECT * FROM  users where email=$1';
+        
+        const result=await client.query(query,["nithinlinga46@gmail.com"]);
         if(result.rows.length>0){
-                        console.log("User found:",result.rows);
-                        return result.rows[0];
-            
-                    }
-                    else{
-                        console.log("User not found:");
-                        return null;
-                    }
+            console.log("User found:",result.rows);
+            return result.rows[0];
+
+        }
+        else{
+            console.log("User not found:");
+            return null;
+        }
 
     } catch (error) {
         console.log(error);
@@ -82,5 +52,35 @@ async function getuserData(){
         await client.end();
     }
 }
+getData().catch(console.error);
 
-getuserData();
+// async function getuserData(){
+//     try {
+//         await  client.connect();
+//         const query=`SELECT u.id,u.firstname,u.lastname,u.email,a.village,a.district,a.state,a.pincode
+//                      FROM users u
+//                      JOIN addresses a ON u.id=a.user_id
+//                      WHERE u.id=1;
+//         `
+
+//         const result=await client.query(query);
+//         if(result.rows.length>0){
+//                         console.log("User found:",result.rows);
+//                         return result.rows[0];
+            
+//                     }
+//                     else{
+//                         console.log("User not found:");
+//                         return null;
+//                     }
+
+//     } catch (error) {
+//         console.log(error);
+//         throw error;
+//     }
+//     finally{
+//         await client.end();
+//     }
+// }
+
+// getuserData();
